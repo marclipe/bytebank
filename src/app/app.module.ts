38927c9265ef3e3be117bf112fd42b06635a383f
marcos.lima <marcos.lima@scaleup.com.br>
2022-11-25 20:18:08 -0300

    Criando o componente de Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NovaTransferencia } from './nova-transferencia/nova-transferencia.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NovaTransferencia],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
