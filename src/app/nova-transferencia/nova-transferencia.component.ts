4792ba52eb4586842f488030a8d01c7b65d9ed16
Marcos Felipe S.L <marcos.lima52@fatec.sp.gov.br>
2022-11-26 17:17:21 -0300

    Recebendo dados no componente

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -6,12 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['nova-transferencia.component.scss'],
 })
 export class NovaTransferencia {
+
+  @Output() aoTransferir = new EventEmitter<any>();
+
   valor: number  = 0; 
   destino: number = 0;
 
   transferir() {
     console.log('Solicitada nova transferência');
-    console.log('Valor: ', this.valor)
-    console.log('Destino: ', this.destino)
+    const valorEmitir = {valor: this.valor, destino: this.destino} 
+    this.aoTransferir.emit(valorEmitir)
+
   }
 }
